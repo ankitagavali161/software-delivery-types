1e63e4c78d7fe43c8c605b96a7cdf002f5c8abd6
Ankita <ankitaofficial161@gmail.com>
2025-11-14 16:27:18 +0100

    linting  test.py

--- src/test.py
+++ src/test.py
@@ -1,10 +1,14 @@
-number_of_images = 59
-default_string = "R.drawable.czas_3__"
-string_to_copy = ""
-end_name = "_,\n"
+"""Generate Android drawable resource strings and save to file."""
 
-for i in range(number_of_images + 1):
-    generated_string = default_string + str(i)
-    string_to_copy += generated_string + end_name
-    with open("string.txt", "w") as file:
-        file.write(string_to_copy)
+NUMBER_OF_IMAGES = 59
+DEFAULT_STRING = "R.drawable.czas_3__"
+INITIAL_STRING = ""
+END_NAME = "_,\n"
+
+RESULT_STRING = INITIAL_STRING
+
+for i in range(NUMBER_OF_IMAGES + 1):
+    GENERATED_STRING = DEFAULT_STRING + str(i)
+    RESULT_STRING += GENERATED_STRING + END_NAME
+    with open("string.txt", "w", encoding="utf-8") as file:
+        file.write(RESULT_STRING)
